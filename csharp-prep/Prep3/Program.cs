cc9e0431b86d4996b9e10ec53ee5e45c7b8c897a
Max Tippetts <maxgtippetts@gmail.com>
2023-04-26 14:43:58 -0600

    Prepped3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,9 +6,19 @@ class Program
     {
         Console.WriteLine("What is the magic number?");
         int num = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Guess:");
-        
-        {Console.WriteLine("Guess:");
+        int guess = -1;
+        while (guess != num)
+        {
+            Console.WriteLine("Guess:");
+            guess = Convert.ToInt32(Console.ReadLine());
+            if (num > guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (num < guess)
+            {
+                Console.WriteLine("Higher");
+            }
         }
     }
 }
