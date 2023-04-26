7d1087ef2f0274e3a051547e3bf59f6bb7e0f583
Max Tippetts <maxgtippetts@gmail.com>
2023-04-26 16:40:08 -0600

    Prepped4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> nums = new List<int>();
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            if (userNumber != 0)
+            {
+                nums.Add(userNumber);
+            }
+        }
+        int sum = nums.Sum();
+        Console.WriteLine($"The sum is {sum}.");
+        int len = nums.Count();
+        int avg = sum/len;
+        Console.WriteLine($"The average is {avg}.");
+        int max = nums[0];
+
+        foreach (int number in nums)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
